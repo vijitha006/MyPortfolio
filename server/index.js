2f99b1f73b2a7b81e7e2c6210e090d681e27a0a8
Vijitha <vijitha006@gmail.com>
2024-04-13 21:24:01 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
    res.header("Access-Control-Allow-Origin", FRONT_END_URI);
+   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   // res.header("Access-Control-Allow-Origin", "*");
   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
